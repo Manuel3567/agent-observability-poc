488ad5281e90107a7779adc7e3fef893d7064e0f
Manuel Schuler <60062851+Manuel3567@users.noreply.github.com>
2025-11-30 16:21:26 +0100

    fix: fixes iam role trust for github repo

--- infra/scripts/bootstrap.sh
+++ infra/scripts/bootstrap.sh
@@ -3,6 +3,6 @@ aws cloudformation deploy \
 --template-file bootstrap/github.yaml \
 --stack-name GitHub-Bootstrap-Stack \
 --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
+--no-fail-on-empty-changeset \
 --parameter-overrides \
-    GitHubRepo=Manuel3567/agent-observability-poc\
---no-fail-on-empty-changeset
+    GitHubRepo=Manuel3567/agent-observability-poc
